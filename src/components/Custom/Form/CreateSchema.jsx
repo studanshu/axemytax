e81e668dbf4802a033df188c68cf476807b35afb
studanshu <bahetysudhanshu@gmail.com>
2024-10-01 23:44:22 -0700

    reverting changes to CreateSchema

--- src/components/Custom/Form/CreateSchema.jsx
+++ src/components/Custom/Form/CreateSchema.jsx
@@ -21,6 +21,15 @@ function flattenNestedEnum(NestedEnumObject, JsonData) {
   }
 }
 
+function getCurrentOptions(data, currentType) {
+  if (currentType === "nestedEnum") {
+    return Object.keys(data.options);
+  } else if (currentType === "enum") {
+    return data.options;
+  }
+  return [];
+}
+
 function updateZodSchemaWithEnum(currentLabel, zodSchema, currentOptions) {
   if (currentLabel in zodSchema) {
     zodSchema[currentLabel] = zodSchema[currentLabel].or(
@@ -31,48 +40,7 @@ function updateZodSchemaWithEnum(currentLabel, zodSchema, currentOptions) {
   }
 }
 
-function getSchemaType(
-  currentType,
-  currentLabel = "Input",
-  currentOptions = [],
-  isRequired = false
-) {
-  let schema = undefined;
-  switch (currentType) {
-    case "text":
-      schema = z
-        .string()
-        .min(5, `${currentLabel} should be atleast 5 characters.`)
-        .max(500, `${currentLabel} should be less than 500 characters`);
-      break;
-    case "email":
-      schema = z.string().email("Invalid Email");
-      break;
-    case "tel":
-      schema = z.string().regex(/^\d{10}$/, "Invalid Phone Number");
-      break;
-    case "enum":
-      schema = z.nativeEnum(currentOptions, enumErrorMap);
-      break;
-    default:
-      schema = z.string();
-  }
-  if (!isRequired) {
-    schema = asOptionalField(schema);
-  }
-  return schema;
-}
-
 const createSchema = (inputData) => {
-  function getCurrentOptions(data, currentType) {
-    if (currentType === "nestedEnum") {
-      return Object.keys(data.options);
-    } else if (currentType === "enum") {
-      return data.options;
-    }
-    return [];
-  }
-
   let JsonData = JSON.parse(JSON.stringify(inputData));
   let zodSchema = {};
   for (let i = 0; i < JsonData.length; i++) {
@@ -81,13 +49,20 @@ const createSchema = (inputData) => {
     let currentOptions = getCurrentOptions(JsonData[i], currentType);
     switch (currentType) {
       case "text":
+        zodSchema[currentLabel] = z
+          .string()
+          .min(5, `${currentLabel} should be atleast 5 characters.`)
+          .max(500, `${currentLabel} should be less than 500 characters`);
+        break;
       case "email":
+        zodSchema[currentLabel] = z.string().email("Invalid Email");
+        break;
       case "tel":
-        zodSchema[currentLabel] = getSchemaType(currentType, currentLabel);
+        zodSchema[currentLabel] = z
+          .string()
+          .regex(/^\d{10}$/, "Invalid Phone Number");
         break;
       case "enum":
-        updateZodSchemaWithEnum(currentLabel, zodSchema, currentOptions);
-        break;
       case "nestedEnum":
         updateZodSchemaWithEnum(currentLabel, zodSchema, currentOptions);
         flattenNestedEnum(JsonData[i], JsonData);
@@ -104,5 +79,4 @@ const createSchema = (inputData) => {
   return zodSchema;
 };
 
-export { getSchemaType };
 export default createSchema;
