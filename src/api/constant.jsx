41bd9ee704b30b443a3f7d25f79693e15852dad8
studanshu <bahetysudhanshu@gmail.com>
2024-10-06 20:40:40 -0700

    adding default value for form endpoint

--- src/api/constant.jsx
+++ src/api/constant.jsx
@@ -1,5 +1,7 @@
 export const Constants = {
   form: {
-    endpoint: process.env.APP_SCRIPT_URL,
+    endpoint:
+      process.env.APP_SCRIPT_URL ||
+      "https://script.google.com/macros/s/AKfycbzeGIjV2acn8weQLTVa3XVfDqgrfwsyWIk1wz7MSeunRmfsIpvpfqNFNV4xFh0rc7HD9g/exec",
   },
 };
