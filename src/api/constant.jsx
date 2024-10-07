41dab0fc0877246753dc849034f195ddfc922eac
studanshu <bahetysudhanshu@gmail.com>
2024-10-06 21:00:15 -0700

    changing endpoint

--- src/api/constant.jsx
+++ src/api/constant.jsx
@@ -2,6 +2,6 @@ export const Constants = {
   form: {
     endpoint:
       process.env.APP_SCRIPT_URL ||
-      "https://script.google.com/macros/s/AKfycbzeGIjV2acn8weQLTVa3XVfDqgrfwsyWIk1wz7MSeunRmfsIpvpfqNFNV4xFh0rc7HD9g/exec",
+      "https://script.google.com/macros/s/AKfycbwDpYyIXkQY3dsBYQqFFFVsTHxR0jst5L2oPBouA7gP597TfwvgPyTlKubA_XZYDHF7iQ/exec",
   },
 };
